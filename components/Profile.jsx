d2065eced3a9661c2a2e0bb14a1bc72abd025acb
PrajwalLuitel <prajwalluitel@gmai.com>
2023-10-22 16:29:44 +0545

    style fixed

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -2,8 +2,8 @@ import PromptCard from "./PromptCard";
 
 const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
   return (
-    <section className="w-full ">
-      <h1 className="head_text text-left">
+    <section className="w-full">
+      <h1 className="head_text overflow-hidden text-left">
         <span className="orange_gradient">{name} Profile</span>
       </h1>
       <p className="desc text-left">{desc}</p>
